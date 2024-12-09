d463d2b3518a54f5dcd18b738dea3d740a823281
“Rikesh-Patel” <rikesh_patel02@yahoo.com>
2024-12-09 00:28:17 -0800

    Update layout

--- src/js/main/zmain.js
+++ src/js/main/zmain.js
@@ -27,15 +27,15 @@
   
   const
   log = console.log,
-  array = ["assets/img/hero_img/1.jpg", "assets/img/hero_img/2.jpg", "assets/img/hero_img/3.jpg", "assets/img/hero_img/5.jpg", "assets/img/hero_img/6.jpg", "assets/img/hero_img/7.jpg"],
+  array = ["assets/img/hero_img/1.jpg", "assets/img/hero_img/2.jpg", "assets/img/hero_img/3.jpg", "assets/img/hero_img/4.jpg",  "assets/img/hero_img/5.jpg", "assets/img/hero_img/6.jpg", "assets/img/hero_img/7.jpg"],
   target = document.getElementById("target_bg_image"),
-  lastSrc = sessionStorage.lastSrc || target.getAttribute("src");
+  lastSrc = sessionStorage.lastSrc || target.style.backgroundImage //target.getAttribute("src");
   let random, newSrc = lastSrc;
   while (newSrc === lastSrc) {
     random = Math.floor(Math.random() * array.length);
-    newSrc = array[random];
+    newSrc = `url('${array[random]}')`; //newSrc = array[random];
   }
-  target.src = sessionStorage.lastSrc = newSrc;
+  target.style.backgroundImage = sessionStorage.lastSrc = newSrc; // target.src = sessionStorage.lastSrc = newSrc;
   log(target);
 
   // Modals
